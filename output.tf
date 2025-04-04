75a4dad927bb27a64fc0e04700c9b85f31fd69f4
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-04 11:34:16 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,1 @@
-output "aws_access_key_id" {
-  value = aws_iam_access_key.aws_cli_user134_key.id
-}
 
-output "aws_secret_access_key" {
-  value     = aws_iam_access_key.aws_cli_user134_key.secret
-  sensitive = true
-}
